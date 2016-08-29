227b6f80ed266660a7a96ee973de6c8059d4e289
Rico Kaltofen <r.kaltofen@heimrich-hannot.de>
2016-08-29 11:07:05 +0200

    moved Form Submission related methods from SubmissionsModel to Haste plus FormSubmission

--- models/SubmissionModel.php
+++ models/SubmissionModel.php
@@ -3,6 +3,7 @@
 namespace HeimrichHannot\Submissions;
 
 use HeimrichHannot\FormHybrid\DC_Hybrid;
+use HeimrichHannot\FormHybrid\Submission;
 use HeimrichHannot\Haste\Dca\General;
 use HeimrichHannot\Haste\Util\Arrays;
 use HeimrichHannot\Haste\Util\Environment;
@@ -18,8 +19,10 @@ class SubmissionModel extends \Model
 
 	public static function getArchiveParent($intSubmission)
 	{
-		if (($objSubmissionArchive = static::getArchive($intSubmission)) !== null &&
-			$objSubmissionArchive->parentTable && $objSubmissionArchive->pid)
+		if (($objSubmissionArchive = static::getArchive($intSubmission)) !== null
+			&& $objSubmissionArchive->parentTable
+			&& $objSubmissionArchive->pid
+		)
 		{
 			if (($objArchiveParent = General::getModelInstance($objSubmissionArchive->parentTable, $objSubmissionArchive->pid)) !== null)
 			{
@@ -95,185 +98,98 @@ class SubmissionModel extends \Model
 
 		if (($objSubmission = SubmissionModel::findByPk($intSubmission)) !== null)
 		{
-			$objDc = new DC_Hybrid('tl_submission');
+			$objDc               = new DC_Hybrid('tl_submission');
 			$objDc->activeRecord = $objSubmission;
-			
+
 			// fields
 			if (($objSubmissionArchive = $objSubmission->getRelated('pid')) !== null)
 			{
 				\Controller::loadDataContainer('tl_submission');
-				
+
 				$arrFields = empty($arrFields) ? deserialize($objSubmissionArchive->submissionFields, true) : $arrFields;
 
-				if (isset($GLOBALS['TL_HOOKS']['preGenerateSubmissionTokens']) &&
-					is_array($GLOBALS['TL_HOOKS']['preGenerateSubmissionTokens'])) {
-					foreach ($GLOBALS['TL_HOOKS']['preGenerateSubmissionTokens'] as $arrCallback) {
+				if (isset($GLOBALS['TL_HOOKS']['preGenerateSubmissionTokens'])
+					&& is_array($GLOBALS['TL_HOOKS']['preGenerateSubmissionTokens'])
+				)
+				{
+					foreach ($GLOBALS['TL_HOOKS']['preGenerateSubmissionTokens'] as $arrCallback)
+					{
 						\System::importStatic($arrCallback[0])->$arrCallback[1]($objSubmission, $objSubmissionArchive, $arrFields);
 					}
 				}
 			}
-			
-			$arrSubmissionData = static::prepareData($objSubmission, 'tl_submission', $GLOBALS['TL_DCA']['tl_submission'], $objDc, $arrFields);
-			
+
+			$arrSubmissionData = static::prepareData($objSubmission, 'tl_submission', $GLOBALS['TL_DCA']['tl_submission'], $objDc, $arrFields, Submissions::getSkipFields());
+
 			$arrTokens = static::tokenizeData($arrSubmissionData);
-			
+
 			// salutation
-			$arrTokens['salutation_submission'] = NotificationCenterPlus::createSalutation($GLOBALS['TL_LANGUAGE'], array(
-				'gender' => $arrTokens['form_value_gender'],
-				'title' => $arrTokens['form_value_title'],
-				'lastname' => $arrTokens['form_value_lastname']
-			));
-			
+			$arrTokens['salutation_submission'] = NotificationCenterPlus::createSalutation(
+				$GLOBALS['TL_LANGUAGE'],
+				array(
+					'gender'   => $arrTokens['form_value_gender'],
+					'title'    => $arrTokens['form_value_title'],
+					'lastname' => $arrTokens['form_value_lastname'],
+				)
+			);
+
 			$arrTokens['tl_submission'] = $objSubmission->id;
 		}
 
 		return $arrTokens;
 	}
 
-	public static function prepareData(\Model $objSubmission, $strTable, array $arrDca, $objDc, $arrFields=array())
+	/**
+	 * @deprecated Use HeimrichHannot\Haste\Util\FormSubmission::prepareData()
+	 * @param \Model $objSubmission
+	 * @param        $strTable
+	 * @param array  $arrDca
+	 * @param        $objDc
+	 * @param array  $arrFields
+	 *
+	 * @return array
+	 */
+	public static function prepareData(\Model $objSubmission, $strTable, array $arrDca = array(), $objDc = null, array $arrFields = array(), array $arrSkipFields = array())
 	{
-		$arrSubmissionData = array();
-		$arrRow = $objSubmission->row();
-
-		if (empty($arrFields))
-			$arrFields = array_keys($arrRow);
-
-		foreach ($arrFields as $strName)
-		{
-			$varValue = $arrRow[$strName];
-			if(empty($varValue)) continue;
-
-			$arrData = $arrDca['fields'][$strName];
-			
-			$arrFieldData = static::prepareDataField($strName, $varValue, $arrData, $strTable, $objDc);
-
-			$arrSubmissionData[$strName] = $arrFieldData;
-			$strSubmission = $arrFieldData['submission'];
-
-			$varValue = deserialize($varValue);
-
-			// multicolumnwizard support
-			if ($arrData['inputType'] == 'multiColumnWizard') {
-				foreach ($varValue as $arrSet) {
-					if (!is_array($arrSet)) {
-						continue;
-					}
-
-					// new line
-					$strSubmission .= "\n";
-
-					foreach ($arrSet as $strSetName => $strSetValue) {
-						$arrSetData   = $arrData['eval']['columnFields'][$strSetName];
-						$arrFieldData = static::prepareDataField($strSetName, $strSetValue, $arrSetData, $strTable, $objDc);
-						// intend new line
-						$strSubmission .= "\t" . $arrFieldData['submission'];
-					}
-
-					// new line
-					$strSubmission .= "\n";
-				}
-			}
-
-			$arrSubmissionData['submission_all'] .= $strSubmission;
-
-			if(in_array($strName, $arrFields) && !in_array($strName, Submissions::getSkipFields()))
-			{
-				$arrSubmissionData['submission'] .= $strSubmission;
-			}
-		}
-
-		return $arrSubmissionData;
+		return FormSubmission::prepareData($objSubmission, $strTable, $arrDca, $objDc, $arrFields, $arrSkipFields);
 	}
 
+	/**
+	 * @deprecated Use HeimrichHannot\Haste\Util\FormSubmission::prepareDataField()
+	 * @param $strName
+	 * @param $varValue
+	 * @param $arrData
+	 * @param $strTable
+	 * @param $objDc
+	 *
+	 * @return array
+	 */
 	public static function prepareDataField($strName, $varValue, $arrData, $strTable, $objDc)
 	{
-		$strLabel = isset($arrData['label'][0]) ? $arrData['label'][0] : $strName;
-
-		$strOutput = FormSubmission::prepareSpecialValueForPrint($varValue, $arrData, $strTable ?: 'tl_submission', $objDc);
-
-		$varValue = deserialize($varValue);
-
-		if (is_array($varValue))
-		{
-			$varValue = Arrays::flattenArray($varValue);
-
-			$varValue = array_filter($varValue); // remove empty elements
-
-			$varValue = implode(', ', $varValue);
-		}
-
-		$strSubmission = $strLabel . ": " . $strOutput . "\n";
-
-		return array('value' => $varValue, 'output' => $strOutput, 'submission' => $strSubmission);
+		return FormSubmission::prepareDataField($strName, $varValue, $arrData, $strTable, $objDc);
 	}
 
+	/**
+	 * @deprecated Use HeimrichHannot\Haste\Util\FormSubmission::tokenizeData()
+	 *
+	 * @param array  $arrSubmissionData
+	 * @param string $strPrefix
+	 *
+	 * @return array
+	 */
 	public static function tokenizeData(array $arrSubmissionData = array(), $strPrefix = 'form')
 	{
-		$arrTokens = array();
-
-		foreach($arrSubmissionData as $strName => $arrData)
-		{
-			if(!is_array($arrData))
-			{
-				if ($strName != 'submission' && $strName != 'submission_all' && !is_object($arrData))
-				{
-					$arrTokens[$strName] = $arrData;
-					continue;
-				}
-				else
-				{
-					continue;
-				}
-			}
-
-			foreach($arrData as $strType => $varValue)
-			{
-				switch($strType)
-				{
-					case 'output':
-						$arrTokens[$strPrefix . '_' . $strName] = $varValue;
-						$arrTokens[$strPrefix . '_plain_' . $strName] =
-							\HeimrichHannot\Haste\Util\StringUtil::convertToText(\StringUtil::decodeEntities($varValue), true);
-						break;
-					case 'value':
-						// check for values causing notification center's json_encode call to fail (unprintable characters like binary!)
-						if (ctype_print($varValue))
-						{
-							$arrTokens[$strPrefix . '_value_' . $strName] = $varValue;
-						}
-						break;
-					case 'submission':
-						$arrTokens[$strPrefix . '_submission_' . $strName] = rtrim($varValue, "\n");
-						break;
-				}
-			}
-		}
-
-		// token: ##formsubmission_all##
-		if(isset($arrSubmissionData['submission_all']))
-		{
-			$arrTokens[$strPrefix . 'submission_all'] = $arrSubmissionData['submission_all'];
-		}
-
-		// token: ##formsubmission##
-		if(isset($arrSubmissionData['submission']))
-		{
-			$arrTokens[$strPrefix . 'submission'] = $arrSubmissionData['submission'];
-		}
-
-		// prepare attachments
-
-
-		return $arrTokens;
+		return FormSubmission::tokenizeData($arrSubmissionData, $strPrefix);
 	}
 
-	public static function generateEntityTokens(\Model $objEntity, array $arrDca, $objDc, $arrFields=array())
+	public static function generateEntityTokens(\Model $objEntity, array $arrDca, $objDc, $arrFields = array())
 	{
 		return static::tokenizeData(static::prepareData($objEntity, 'tl_submission', $arrDca, $objDc, $arrFields));
 	}
 
 	/**
 	 * Creates a new submission in a certain archive and assigns a logged in member (if existing)
+	 *
 	 * @param $intPid
 	 * @param $intMember
 	 *
@@ -281,9 +197,9 @@ class SubmissionModel extends \Model
 	 */
 	public static function create($intPid, $intMember = null)
 	{
-		$objSubmission = new static();
-		$objSubmission->pid = $intPid;
-		$objSubmission->dateAdded = time();
+		$objSubmission               = new static();
+		$objSubmission->pid          = $intPid;
+		$objSubmission->dateAdded    = time();
 		$objSubmission->memberAuthor = $intMember;
 
 		$objSubmission->save();
