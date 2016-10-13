8035422597609a3ca10f52577690531338717785
Dennis Patzer <d.patzer@heimrich-hannot.de>
2016-10-13 15:46:43 +0200

    added language

---
+++ languages/en/tl_nc_notification.php
@@ -0,0 +1,7 @@
+<?php
+
+$arrLang = &$GLOBALS['TL_LANG']['tl_nc_notification'];
+
+$arrLang['type'][\HeimrichHannot\Submissions\Submissions::NOTIFICATION_TYPE_SUBMISSIONS] = 'Submissions';
+$arrLang['type'][\HeimrichHannot\Submissions\Submissions::NOTIFICATION_TYPE_FORM_SUBMISSION] = array('Form Submission', 'asd');
+$arrLang['type'][\HeimrichHannot\Submissions\Submissions::NOTIFICATION_TYPE_CONFIRMATION] = array('Confirmation', 'asd');
