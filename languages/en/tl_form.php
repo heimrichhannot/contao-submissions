1ebd8b2525cab22cdc6d6a84f327457b20d42d7a
Thomas Körner <t.koerner@heimrich-hannot.de>
2022-02-02 14:30:53 +0100

    added event, updated some translations

--- languages/en/tl_form.php
+++ languages/en/tl_form.php
@@ -5,9 +5,9 @@ $lang = &$GLOBALS['TL_LANG']['tl_form'];
 $lang['storeAsSubmission'] = ['Store as submission', "Save submitted form data as submission. This option overrides the \"store data\" option!"];
 $lang['submissionArchive'] = ['Submission archive', "The submissin archive which should store the submission data."];
 
-$lang['huhSubAddOptIn']          = ['Activate opt-in', "Activate opt-in process for form submissions."];
-$lang['huhSubOptInNotification'] = ['Opt-in notification', "Choose an opt-in notification."];
-$lang['huhSubOptInJumpTo']       = ['Opt-in redirect page', "Choose a page to which the visitor will be redirected after successful opt-in."];
+$lang['huhSubAddOptIn']          = ['Activate double opt-in', "Activate double opt-in process for form submissions."];
+$lang['huhSubOptInNotification'] = ['Double opt-in notification', "Choose an opt-in notification."];
+$lang['huhSubOptInJumpTo']       = ['Double opt-in success redirect page', "Choose a page to which the visitor will be redirected after successful opt-in."];
 $lang['huhSubOptInTokenInvalidJumpTo'] = ['Token confirmed page', "Choose a page the visitor will be redirected to if the token is already confirmed."];
-$lang['huhSubOptInField']        = ['Opt-in confirmation field', "Choose a field that should set to true after a successful opt-in."];
+$lang['huhSubOptInField']        = ['Double opt-in confirmation field', "Choose a field that should set to true after a successful double opt-in."];
 
